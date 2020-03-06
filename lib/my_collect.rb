83e7f69a41686075d2835476ac88a92c35c00ab9
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-06 06:23:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -33,3 +33,13 @@
 #     new_array.push(language.upcase)
   
 # end
+
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
