ce9e89bc0f562a76b8abd70c1a57a33da3ffbe6f
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-06 06:07:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,7 +9,7 @@ def my_collect(array)
   while i<array.length do
     yield(array[i])
     return new_array.push(array[i].split(" ").first)
-    return new_array.push(array[i].upcase)
+    
     
     i+=1 
   end
