003b16a1f45b150e15f4ce088a8bad40feadf265
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-06 05:39:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,31 @@
+# collection=["Jim Jones","Anil G","Alina G"]
+
+def my_collect(array)
+  new_array=[]
+  
+  i=0 
+  while i<array.length do
+    yield(array[i])
+    
+    if array[i].split(" ").first.size>1
+      new_array.push(array[i].split(" ").first)
+    else
+      new_array.push(array[i].upcase)
+    end
+    
+    i+=1 
+    
+  end
+  new_array
+end
 
 
+# if collection[0].split(" ").size>1 
+#   my_collect(collection) do |first|
+#     new_array.push(first.split(" ").first)
+ 
+# else
+#   my_collect(collection) do |language| 
+#     new_array.push(language.upcase)
+  
+# end
